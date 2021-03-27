b714e457d4c2027e27c0ef0a4f1b7f3cc782f50b
LJH's MacBookPro16 <joonhyuklee@MacBook-Pro-2.local>
2021-03-27 22:57:02 +0900

    Update game.ts

--- game.ts
+++ game.ts
@@ -1,6 +1,9 @@
 /**
  * Let's make a game 🕹
  */
+
+const position = { x: 0, y: 0};
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
@@ -10,3 +13,30 @@ move('left');
 console.log(position); // { x: -1, y: 0}
 move('right');
 console.log(position); // { x: 0, y: 0}
+
+type Direction = 'up'|'down'|'left'|'right';
+
+type Point = { 
+    x: number
+    y: number
+}
+
+function move(direction : Direction ): void | never {
+    switch(direction){
+        case 'up' :
+            ++position.y;
+            break; // 아래 case를 실행 x
+        case 'down' :
+            --position.y;
+            break;
+        case 'left' :
+            --position.x;
+            break;
+        case 'right' :
+            ++position.x;
+            break;
+        default:
+            throw new Error(`unknown direction : ${direction}`);//오류 발생시 어떤 direction인지 알려줌
+    }
+
+};
